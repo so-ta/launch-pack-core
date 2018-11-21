3d591765ace4fe92917be74172c76af04d8d1aab
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2018-11-22 00:18:30 +0900

    configure eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
   "extends": "airbnb-base",
+  "rules": {
+    "default-case": "off",
+    "import/no-dynamic-require": "off",
+    "no-console": "off",
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-underscore-dangle": "off",
+  },
+  "overrides": [
+    {
+      "files": "bin/*.js",
+      "parserOptions": {
+        "sourceType": "script",
+      },
+    },
+  ],
 };
