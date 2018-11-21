cc5d5076cd03d1723bf6f4373573418d76e514da
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2018-11-21 16:45:21 +0900

    refactor scripts

--- bin/launch-pack.js
+++ bin/launch-pack.js
@@ -4,18 +4,7 @@
 
 const server = require('../lib/server');
 
-console.log('LaunchPack Launch');
-
-// Set env var for ORIGINAL cwd
-// before anything touches it
 const cwd = process.cwd();
-
-// Exit with 0 or 1
-const failed = false;
-process.once('exit', (code) => {
-  if (code === 0 && failed) {
-    process.exit(1);
-  }
-});
-
 server.launchServer(cwd);
+
+console.info('LaunchPack Launch');
