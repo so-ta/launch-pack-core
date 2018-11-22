f150a3465b5161f9032cc74cfb569bac1099d621
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2018-11-22 17:30:05 +0900

    fix scripts automatically using eslint
    just ran `yarn fix:js`

--- bin/launch-pack.js
+++ bin/launch-pack.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict';
 
 const server = require('../lib/server');
@@ -7,11 +8,11 @@ console.log('LaunchPack Launch');
 
 // Set env var for ORIGINAL cwd
 // before anything touches it
-let cwd = process.cwd();
+const cwd = process.cwd();
 
 // Exit with 0 or 1
-let failed = false;
-process.once('exit', function (code) {
+const failed = false;
+process.once('exit', (code) => {
   if (code === 0 && failed) {
     process.exit(1);
   }
