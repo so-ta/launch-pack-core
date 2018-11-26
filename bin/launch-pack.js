ec565381f4abdfa6adae2a39e384ba6511faed7e
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2018-11-26 13:36:37 +0900

    modify running mode setting

--- bin/launch-pack.js
+++ bin/launch-pack.js
@@ -2,4 +2,14 @@
 
 'use strict';
 
+global.LaunchPack = {};
+
+// 実行モードの設定
+const eunModeIndex = 2;
+if (eunModeIndex < process.argv.length) {
+  global.LaunchPack.RUN_MODE = process.argv[eunModeIndex];
+} else {
+  global.LaunchPack.RUN_MODE = 'local'; // デフォルトの設定
+}
+
 require('../lib/server').launchServer(process.cwd());
